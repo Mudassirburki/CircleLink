8d110ce4ed1ee8d67dedcc71982ca9c64e686d2b
Mudassirburki <mudassirburki762@gmail.com>
2026-04-08 14:26:45 +0500

    set up done

---
+++ src/screens/auth/LoginScreen.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>LoginScreen</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+})
